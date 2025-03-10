a5371fb039cbb5a65ca39b43e3e127262b02ceb2
chensheep <chensheep1005@gmail.com>
2025-03-10 10:01:23 +0800

    Implement q_remove_head and q_remove_tail
    - q_remove_head removes a node from the head of the queue.
    - q_remove_tail removes a node from the tail of the queue.
    
    Change-Id: I7e592341f51d6f32ff6656b1d76fd15e2ed53925

--- queue.c
+++ queue.c
@@ -59,13 +59,41 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+
+    if (list_empty(head))
+        return NULL;
+
+    element_t *first;
+    first = list_first_entry(head, element_t, list);
+
+    if (!first)
+        return NULL;
+
+    strncpy(sp, first->value, bufsize - 1);
+    sp[bufsize - 1] = '\0';
+    list_del(&first->list);
+
+    return first;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+
+    if (list_empty(head))
+        return NULL;
+
+    element_t *last;
+    last = list_last_entry(head, element_t, list);
+
+    strncpy(sp, last->value, bufsize - 1);
+    list_del(&last->list);
+
+    return last;
 }
 
 /* Return number of elements in queue */
